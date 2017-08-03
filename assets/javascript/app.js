a2328eec022dbcfabf48109dc94878e39a280f51
Julian K. Bridges <julianbridges01@gmail.com>
2017-08-02 21:38:29 -0400

    deleted alerts and console.logs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,9 +9,6 @@ function displayGif() {
 		url: queryURL,
 		method: "GET"
 	}).done(function(response) {
-		console.log(response);
-		// $(".gif-view").append("<div class='gif-display'>");
-		// trying to create a variable for the gif
 		for (i = 0; i < response.data.length; i++) {
 			var gifObject = response.data[i];
 			var animatedGif = gifObject.images.fixed_height.url;
@@ -21,7 +18,6 @@ function displayGif() {
 			var gifHeader = $("<h3>").text("Rating: " + ratingGif).addClass("rating-header");
 			var gif = $("<img>").attr("src", pausedGif).attr("data-paused", pausedGif).attr("data-animated", animatedGif).addClass("gif-hover");
 			$(".gif-display").last().append(gifHeader, gif);
-			console.log(pausedGif);
 		}
 	});
 }
@@ -43,7 +39,6 @@ $(".add-sport").on("click", function(event) {
 	var sport = $(".searchForm").val().trim();
 	if (sports.indexOf(sport.toLowerCase()) === -1) {
 		sports.push(sport.toLowerCase());
-		alert("true");
 		renderButtons();
 	}
 });
